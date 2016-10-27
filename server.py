348653f70fd2d658e046d30a40dd6bd2fdf4e014
Kory Schneider <kory.schneider@gmail.com>
2016-10-27 12:12:09 -0700

    Fix server

--- server.py
+++ server.py
@@ -1,8 +1,15 @@
-import logging
-import flask
-from flask import render_template
+import flask, logging
+
+# Globals
+app = flask.Flask(__name__)
+import CONFIG
 
 @app.route('/')
 @app.route('/index')
 def index():
     return flask.render_template('index.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.logger.setLevel(logging.DEBUG)
+    app.run(port=CONFIG.PORT, host='0.0.0.0')
